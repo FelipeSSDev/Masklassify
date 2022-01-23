cccc629fbca5b0ae315d4c404ca836f9c52547a9
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-23 20:20:06 -0300

    feat: Add cnpj tests

---
+++ __tests__/masks/cnpj.spec.ts
@@ -0,0 +1,26 @@
+import {cnpj} from '../../src/masks';
+
+describe('Test CNPJ Mask', () => {
+  const sut = cnpj;
+
+  it('should received CEP as a defined object', () => {
+    expect(sut).toBeDefined();
+  });
+
+  it('should transform a CNPJ into a digits string with no formatting', () => {
+    expect(sut.raw('11.444.777/0001-33')).toBe('11444777000133');
+  });
+
+  it('should validate a CNPJ with 14 digits', () => {
+    expect(sut.validate('97.907.999/0001-99')).toBeTruthy();
+  });
+
+  it('should fail to validate a CNPJ', () => {
+    expect(sut.validate('11.444.777/0001-3')).toBeFalsy();
+    expect(sut.validate('11.444.777/0001-34')).toBeFalsy();
+  });
+
+  it('should format a CNPJ', () => {
+    expect(sut.value('11444777000133')).toBe('11.444.777/0001-33');
+  });
+});
