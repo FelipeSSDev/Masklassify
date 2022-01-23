38e4f87a3cc7ee447e5b36d4a2e44cfabba669f0
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-23 19:59:54 -0300

    feat: Add cep tests

---
+++ __tests__/masks/cep.spec.ts
@@ -0,0 +1,25 @@
+import {cep} from '../../src/masks';
+
+describe('Test CEP Mask', () => {
+  const sut = cep;
+
+  it('should received CEP as a defined object', () => {
+    expect(sut).toBeDefined();
+  });
+
+  it('should transform a CEP into a digits string with no formatting', () => {
+    expect(sut.raw('12345-678')).toBe('12345678');
+  });
+
+  it('should validate a CEP with 9 digits', () => {
+    expect(sut.validate('12345-678')).toBeTruthy();
+  });
+
+  it('should fail to validate a CEP', () => {
+    expect(sut.validate('12345-67')).toBeFalsy();
+  });
+
+  it('should format a CEP', () => {
+    expect(sut.value('12345678')).toBe('12345-678');
+  });
+});
