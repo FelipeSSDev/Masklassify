a0faa90cff49087694e4e6106d4e5be55763edf4
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-27 16:53:38 -0300

    fix: Refactor toPattern function

--- src/masks/custom.ts
+++ src/masks/custom.ts
@@ -1,60 +1,69 @@
-const TRANSLATION: Record<string, (s: string) => string> = {
+const TRANSLATION: Record<string, (s: string) => string | null> = {
   '*': val => val,
   '9': val => val.replace(/[^0-9]+/g, ''),
   A: val => val.replace(/[^a-zA-Z]+/g, ''),
   S: val => val.replace(/[^a-zA-Z0-9]+/g, ''),
 };
 
-const toPattern = (value: string, mask: string) => {
+const maskValue = (value: string, mask: string) => {
   let result = '';
   let maskCharIndex = 0;
   let valueCharIndex = 0;
 
   while (true) {
-    if (maskCharIndex === mask.length) break;
-    if (valueCharIndex === value.length) break;
+    if (mask.length === maskCharIndex) break;
+    if (value.length === valueCharIndex) break;
 
-    const maskChar = mask[maskCharIndex];
-    const valueChar = value[valueCharIndex];
+    const currentMaskCharacter = mask[maskCharIndex];
+    const currentValueCharacter = value[valueCharIndex];
+    const translationHandler = TRANSLATION[currentMaskCharacter];
 
-    // value equals mask, just set
-    if (maskChar === valueChar) {
-      result += maskChar;
-      valueCharIndex += 1;
-      maskCharIndex += 1;
+    if (currentValueCharacter === currentMaskCharacter) {
+      result += currentMaskCharacter;
+
+      valueCharIndex++;
+      maskCharIndex++;
+
       continue;
     }
-
-    // apply translator if match
-    const translationHandler = TRANSLATION[maskChar];
 
     if (!translationHandler) {
-      // not masked value, fixed char on mask
-      result += maskChar;
-      maskCharIndex += 1;
+      result += currentMaskCharacter;
+
+      maskCharIndex++;
+
       continue;
     }
 
-    const resolverValue = translationHandler(valueChar || '');
+    const translatedValue = translationHandler(currentValueCharacter || '');
 
-    if (resolverValue === '') {
-      // valueChar replaced so don't add it to result, keep the mask at the same point and continue to next value char
-      valueCharIndex += 1;
+    if (translatedValue === '') {
+      valueCharIndex++;
+
       continue;
-    } else if (resolverValue !== null) {
-      result += resolverValue;
-      valueCharIndex += 1;
-    } else {
-      result += maskChar;
     }
 
-    maskCharIndex += 1;
+    if (translatedValue !== null) {
+      result += translatedValue;
+
+      valueCharIndex++;
+      maskCharIndex++;
+
+      continue;
+    }
+
+    result += currentMaskCharacter;
+
+    maskCharIndex++;
   }
+
   return result;
 };
 
 export const value = (value = '', mask = '') => {
   if (value === '') return value;
-  const masked = toPattern(value, mask);
-  return masked;
+
+  const maskedValue = maskValue(value, mask);
+
+  return maskedValue;
 };
